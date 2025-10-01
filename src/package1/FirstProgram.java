d2eecd7774401b97ba6ff7fb7537911904ec7068
Vidhyashree <vidhyashreelm@gmail.com>
2025-10-01 13:29:37 +0530

    First Commit

---
+++ src/package1/FirstProgram.java
@@ -0,0 +1,19 @@
+package package1;
+
+public class FirstProgram {
+
+	public FirstProgram() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		Sample1 s = new Sample1();   //# to access a variable or method from other class i.e, from Sample.java
+        s.display();
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/package1/Sample1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Sample1 {
+
+
+	int a;   //data member
+	int b;   //data member
+	
+	void display() {
+		System.out.println("Value of a and b is "+a+" "+b+"");  //Member functions
+	}
+}
