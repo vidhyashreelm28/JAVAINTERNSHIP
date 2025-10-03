3ff60af99075c3a1584335ec999048d0ed724ce1
Vidhyashree <vidhyashreelm@gmail.com>
2025-10-03 11:52:48 +0530

    conent variation

--- src/package1/FirstProgram.java
+++ src/package1/FirstProgram.java
@@ -7,7 +7,7 @@ public class FirstProgram {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello ewit");
 		Sample1 s = new Sample1();   //# to access a variable or method from other class i.e, from Sample.java
         s.display();
 
