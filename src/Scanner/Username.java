1029f8130b10454db5563b87e6f6d67ebf50d947
Vidhyashree <vidhyashreelm@gmail.com>
2025-10-07 18:00:53 +0530

    Scanner

---
+++ src/Scanner/Username.java
@@ -0,0 +1,25 @@
+package Scanner;
+import java.util.Scanner;
+
+public class Username {
+
+	public Username() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name=sc.nextLine();
+		System.out.println("Enter your address");
+		String address=sc.next();
+		System.out.println("Enter your phone number");
+		long phone=sc.nextLong();
+		System.out.println("Enter the gender by typing F or M ");
+		char gender=sc.next().charAt(0);
+		System.out.println("Hello" +name+ ""+address+ "CONTACT THROUGH"+phone);
+		System.out.println("Gender is"+gender);
+
+	}
+
+}
