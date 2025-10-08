c2ab9b6eb7f2b0dde05aae4b38137e8f6749d993
Vidhyashree L M <vidhyashreelm@gmail.com>
2025-10-08 20:55:12 +0530

    Update SingleInheritance.java

--- src/SingleInheritance.java
+++ src/SingleInheritance.java
@@ -1,12 +1,1 @@
-package Inheritance;
 
-public class SingleInheritance {
-
-	public static void main(String[] args)
-	{
-		Student obj =new Student("Sanjay","A0123","Bangalore",1236L,123,"Ewit"); //why student becuase student has both citizen and student properties.......
-		System.out.println(obj);
-	}
-	
-
-}
